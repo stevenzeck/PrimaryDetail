7b90438298c6672e6edd0bb3bf3a10c09aa08b08
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2024-04-13 00:41:38 -0500

    Updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,7 @@ dependencies {
     implementation(libs.bundles.retrofit)
 
     // Kotlin Serialziation
-    implementation(libs.bundles.kotlin.serialization)
+    implementation(libs.kotlin.serialization.json)
 
     // Hilt
     implementation(libs.hilt.android)
